9918fef17886ce3658a20c230ffb583cb66ad574
RupakGhosh4865 <rupak4865@gmail.com>
2024-09-10 23:46:01 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [ "https://frontend-playtube.vercel.app/"],
+    origin: [ "https://backend-playtubes.onrender.com/api/v1"],
     credentials: true,
   })
 );
